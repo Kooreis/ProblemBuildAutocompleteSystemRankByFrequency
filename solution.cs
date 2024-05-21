0e2f9d38cbd548083860d296b81b85908b44716d
Kooreis <kooreis.official@gmail.com>
2024-05-21 18:16:13 -0500

    This update introduces the Trie class with a constructor and an insert method. The constructor initializes the Trie with a root node. The insert method adds a word to the Trie, creating new nodes as necessary, and increments the count of the word.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,20 @@
-class TrieNode {
+class Trie {
     constructor() {
-        this.children = {};
-        this.endOfWord = false;
-        this.count = 0;
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let current = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let ch = word.charAt(i);
+            let node = current.children[ch];
+            if (node == null) {
+                node = new TrieNode();
+                current.children[ch] = node;
+            }
+            current = node;
+        }
+        current.endOfWord = true;
+        current.count++;
     }
 }
